76e579de7a6043674209bf067d68117979480db2
Victor Luís <victorluiscaguiar@gmail.com>
2025-07-08 15:27:02 -0300

    feat: implement coin flip command

---
+++ commands/gambling/flip.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+function flip() {
+  return Math.floor(Math.random() * 2);
+}
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("flip")
+    .setDescription("Randomly flip a coin to get head or tails."),
+  async execute(interaction) {
+    const result = flip();
+
+    if (result === 0) {
+      await interaction.reply(
+        `${interaction.user.username} flipped a coin: HEADS`
+      );
+    } else {
+      await interaction.reply(
+        `${interaction.user.username} flipped a coin: TAILS`
+      );
+    }
+  },
+};
